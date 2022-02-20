c102484c631defbb43f64c70bd4330c049d2024e
djalexkidd <alexpyr@outlook.fr>
2022-02-20 23:07:44 +0100

    Feu multidirectionnel

--- scripts/Fire.gd
+++ scripts/Fire.gd
@@ -1,24 +1,23 @@
 extends KinematicBody2D
 
 var velocity = Vector2()
-var speed = 700
+const speed = 15
 var orientation
+onready var mouse_pos = null
 
 func _ready():
 	set_position(get_node("/root/Node2D/Player").get_position())
+	mouse_pos = get_local_mouse_position()
 	if get_node("../Player/AnimatedSprite").flip_h:
 		orientation = true
 		$Sprite.flip_h = true
 	else:
 		orientation = false
-		
 
 func _physics_process(delta):
-	if orientation:
-		velocity.x = -speed
-	else:
-		velocity.x = speed
-	velocity = move_and_slide(velocity)
+	velocity = velocity.move_toward(mouse_pos, delta)
+	velocity = velocity.normalized() * speed
+	position = position + velocity
 
 func _on_Area2D_body_entered(body):
 	body.explode()
